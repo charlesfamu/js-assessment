b50e3e8771690176b33d42013e795cbcfcd78a3c
Charles Bunton <charles.bunton@buntonco.com>
2017-01-20 15:50:02 -0500

    updated arrays.js and objects.js

--- app/arrays.js
+++ app/arrays.js
@@ -3,51 +3,90 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+      return arr.reduce(function (a, b) {
+        return a + b;
+      }, 0);
     },
 
     remove : function(arr, item) {
+      while( arr.indexOf(item) !== -1 ) {
+        var index = arr.indexOf(item);
+        arr.splice(index, 1);
+      }
 
+      return arr;
     },
-    
+
     removeWithoutCopy : function(arr, item) {
 
     },
 
     append : function(arr, item) {
-
+      arr.push(item);
+      return arr;
     },
 
     truncate : function(arr) {
-
+      arr.pop();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
+      var arr1Str = arr1.map(function(val) {
+        return val.toString();
+      });
 
+      var arr2Str = arr2.map(function(val) {
+        return val.toString();
+      });
+
+      return arr1Str.concat(arr2Str);
     },
 
     insert : function(arr, item, index) {
-
+      arr.splice(index, 0, item);
+      return arr;
     },
 
     count : function(arr, item) {
+      var count = 0;
+      while ( arr.indexOf(item) !== -1 ) {
+        count = count + 1;
+        var index = arr.indexOf(item);
+        arr.splice(index, 1);
+      }
 
+      return count;
     },
 
     duplicates : function(arr) {
-
+      var result = [];
+      var seen = [];
+      for (var i = 0; i < arr.length; i++) {
+        if (seen.indexOf(arr[i]) > -1) {
+          result.push(arr[i]);
+        }
+        seen.push(arr[i]);
+      }
+      return result;
     },
 
     square : function(arr) {
-
+      return arr.map(function (a) {
+        return a * a;
+      });
     },
 
     findAllOccurrences : function(arr, target) {
-
+      var count = 0;
+      for (var i = 0; i < arr.length; i++) {
+        count = arr[i] == target ? count + 1 : count;
+      }
+      return count;
     }
   };
 });
--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      return constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
+      var result = [];
 
+      for(var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          var keyValStr = '' + prop + ': ' + obj[prop] + '';
+          result.push(keyValStr);
+        }
+      }
+
+      return result;
     }
   };
 });
